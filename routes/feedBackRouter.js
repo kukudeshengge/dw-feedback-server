a16a12004e781cff47064de77d3995a7a8229b82
纪永升 <jiyongsheng@idongwo.com>
2023-12-21 19:44:31 +0800

    code

--- routes/feedBackRouter.js
+++ routes/feedBackRouter.js
@@ -12,7 +12,7 @@ router.post('/save', function (req, res) {
     const newList = list ? JSON.parse(list) : []
     if (!desc || desc?.trim() === '') {
       return res.json({
-        code: 500,
+        code: '500',
         message: '参数有误'
       })
     }
@@ -20,12 +20,12 @@ router.post('/save', function (req, res) {
     newList.push({ name, desc, fileList, submitTime })
     fs.writeFileSync(listPath, JSON.stringify(newList), 'utf-8')
     res.json({
-      code: 200,
+      code: '200',
       message: '保存成功'
     })
   } catch (err) {
     res.json({
-      code: 500,
+      code: '500',
       message: err.message
     })
     console.log(err)
@@ -35,19 +35,19 @@ router.post('/save', function (req, res) {
 router.get('/clear', function (req, res) {
   if (!req.query.token) {
     return res.json({
-      code: 401,
+      code: '401',
       token: '执行清除操作需携带token'
     })
   }
   if (req.query.token !== '15612868761') {
     return res.json({
-      code: 401,
+      code: '401',
       token: 'token有误'
     })
   }
   fs.writeFileSync(listPath, JSON.stringify([]))
   res.json({
-    code: 200,
+    code: '200',
     message: '清除成功'
   })
 })
